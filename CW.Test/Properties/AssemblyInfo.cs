5b27d2346e1c1a5e894174bbbcbb9a585025579a
Viktor <viktor.popov@gmail.com>
2017-09-06 10:02:00 -0500

    Added test

---
+++ CW.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("CW.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("CW.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("e23a8e2a-06cd-4d55-a92b-3f92a2b09074")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ CW.Test/UnitTest1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CW.API;
+
+namespace CW.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        [ExpectedException(typeof(Exception))]
+        public void WrongClassThrowsException()
+        {
+            var writer = WriterFactory.GetWriter("Test");
+        }
+
+        [TestMethod]
+        public void WriterFactoryReturnsCorrectWriter()
+        {
+            var writer = WriterFactory.GetWriter("CW.API.ConsoleWriter,CW.API");
+            Assert.IsInstanceOfType(writer, typeof(IWriter));
+        }
+
+        [TestMethod]
+        public void ConsoleWriterWritesWOException()
+        {
+            var writer = WriterFactory.GetWriter("CW.API.ConsoleWriter,CW.API");
+            try
+            {
+                writer.Write("test");
+            }
+            catch (Exception ex)
+            {
+                Assert.Fail("Expected no exception, but got: " + ex.Message);
+            }
+        }
+    }
+}
